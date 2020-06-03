765e97598a9c4f5155969590c940f80206a3a14e
xxz <1316143408@qq.com>
2020-06-03 18:44:56 +0800

    登录界面完成

--- pages/my/index.js
+++ pages/my/index.js
@@ -1,55 +1,38 @@
 
 const user = require("../../api/login/index.js")
 // pages/my/index.js
-Component({
- 
+Page({
+
   /**
    * 页面的初始数据
    */
   data: {
-    loginLoading:false,
-    buttonType:"warn",
-    phone:"",
-    password:"",
-    isPassword:true
+    userInfo:"", //用户信息
+    isLogin:false //是否登录
   },
-  methods:{
-    //登录
-    handleLogin(){
-      let data = {
-        phone:this.data.phone,
-        password: this.data.password
-      }
-      user.login(data).then(response =>{
-        console.log(response)
-      })
-    },
-    //设置手机号码
-    handleSetPhone(e){
-      this.setData({
-        phone:e.detail.value
-      })
-    },
-    //设置登录密码
-    handleSetPwd(e){
-      this.setData({
-        password:e.detail.value
-      })
-    }
-  },
+
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    const userInfo = wx.getStorageSync("wxUserInfo")
+    if(userInfo){
+      this.setData({
+        userInfo:userInfo
+      })
+      this.setData({
+        isLogin: true
+      })
+    }else{
+      this.handleGetAuthorize()
+    }
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
-   *
    */
   onReady: function () {
-
+    
   },
 
   /**
@@ -68,34 +51,97 @@ Component({
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+    
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
+    
+  },
+  /**
+   * 用户授权
+   */
+  handleGetAuthorize(){
+    const _that = this
+    wx.getSetting({
+      success(res){
+        if (!res.authSetting['scope.userInfo']){
+          wx.authorize({
+            scope: 'scope.userInfo',
+            success(){
+              wx.showToast({
+                title: '成功',
+                icon:"success",
+                duration:1000
+              })
+              _that.handleGetUserInfo()
+              this.setData({
+                isLogin: false
+              })
+            },
+            fail(){
+              wx.showModal({
+               title: '警告',
+                content:"您点击了拒绝授权,点击确定重新获取授权。",
+               success(res){
+                 if (res.confirm){
+                   wx.openSetting({
+                     success(res){
+                       this.setData({
+                         isLogin: false
+                       })
+                     }
+                   })
+                 }
+               }
+             })
+            }
+          })
+        }else {
+          _that.handleGetUserInfo()
+        }
+      }
+    })
+  },
+  /**
+   * 获取用户信息
+   */
+  handleGetUserInfo(){
+    const _that = this
+    this.setData({
+      isLogin:true
+    })
+    wx.getUserInfo({
+      lang:"zh-CN",
+      success(res){
+        _that.setData({
+          userInfo:res.userInfo
+        })
+        wx.setStorageSync("wxUserInfo", res.userInfo)
+      }
+    })
   }
 })
